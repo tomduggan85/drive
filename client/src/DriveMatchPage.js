de35293b22a3680435f02fe4a08fbdfb8c504435
Tom Duggan <tduggan@squarespace.com>
2018-12-14 10:22:52 -0500

    websocket connection to nowhere

--- client/src/DriveMatchPage.js
+++ client/src/DriveMatchPage.js
@@ -3,11 +3,16 @@ import './DriveMatchPage.css';
 import DriveScene from './DriveScene';
 import CameraRenderer from './CameraRenderer';
 
+const WEBSOCKET_PORT = 8080
+
 class DriveMatchPage extends React.Component {
 
   constructor( props ) {
     super( props );
     this.driveScene = new DriveScene();
+
+    const { matchId } = props.match.params;
+    this.socket = new WebSocket(`ws://${ window.location.hostname }:${ WEBSOCKET_PORT }/match/${ matchId }`);
   }
 
   render() {
