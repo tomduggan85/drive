9328dc1635648bd82aed9c823e4232e4574708a7
Tom Duggan <tduggan@squarespace.com>
2018-12-15 11:36:02 -0500

    clean up ngrok comments and code

--- client/src/shared/DriveMatchSocket.js
+++ client/src/shared/DriveMatchSocket.js
@@ -1,18 +1,23 @@
 
-const PORT = 8080
+const PORT = 8080;
+
+const USE_NGROK_TEST = false;
+
+/*
+    ngrok / phone remote-control testing instructions for localhost
+    THis assumes pro ngrok plan, which lets you choose subdomain.  If free plan, adjust below urls with ngrok-provided subdomains
+
+    1) in terminal window: ./ngrok http -subdomain drivegame-socket 8080
+    2) in another terminal window: ./ngrok http -subdomain drivegame 3000
+    3) on laptop, http://localhost:3000/match/1
+    4) on phone, http://drivegame.ngrok.io/match/1/remote
+  */
 
 export const getSocket = ( matchId ) => {
+
+  if ( USE_NGROK_TEST ) {
+    return new WebSocket(`ws://drivegame-socket.ngrok.io/match/${ matchId }`);
+  }
   
-  //return new WebSocket(`ws://${ window.location.hostname }:${ PORT }/match/${ matchId }`);
-
-
-  /*
-    NGROK / phone remote-control:
-    1) ./ngrok http -subdomain td-socket 8080
-    2) ./ngrok http -subdomain td-test 3000
-
-    3) on laptop, http://localhost:3000/match/1
-    4) on phone, http://td-test.ngrok.io/match/1/remote
-  */
-  return new WebSocket(`ws://td-socket.ngrok.io/match/${ matchId }`);
+  return new WebSocket(`ws://${ window.location.hostname }:${ PORT }/match/${ matchId }`);
 }
