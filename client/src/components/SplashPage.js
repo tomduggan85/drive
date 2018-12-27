96fb54947566ffb5993d70db6b25fc2eb91fe64b
Tom Duggan <tduggan@squarespace.com>
2018-12-27 12:35:05 -0500

    split into two pages

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import uuid from 'uuid';
-import { VEHICLE_TYPES } from '../shared/Vehicles';
 import './SplashPage.css';
-import classnames from 'classnames'
 
 class SplashPage extends React.Component {
-
-  state = {
-    matchId: uuid().substr(0, 5), //hacky way to generate a random matchId on each splash page mount.
-    selectedVehiclePlayer1: VEHICLE_TYPES[0], 
-    selectedVehiclePlayer2: VEHICLE_TYPES[1],
-    showVehicleSelection: false,
-  }
-
-  startMatch = () => {
-    const { selectedVehiclePlayer1, selectedVehiclePlayer2, matchId } = this.state;
-    this.props.history.push( `/match/${ matchId }?v1=${ selectedVehiclePlayer1 }&v2=${ selectedVehiclePlayer2 }` );
-  }
 
   selectVehicle = ( playerIndex, vehicleType ) => {
     this.setState({
@@ -24,58 +9,21 @@ class SplashPage extends React.Component {
     })
   }
 
-  startSelection = () => {
-    this.setState({ showVehicleSelection: true })
+  start = () => {
+    this.props.history.push('/select-vehicle');
   }
 
-  renderVehicleSelector( playerIndex ) {
-    const currentSelection = this.state[`selectedVehiclePlayer${ playerIndex }`];
-
-    return (
-      <div className='vehicle-selector'>
-        Player { playerIndex } vehicle:
-        {VEHICLE_TYPES.map( vehicleType => (
-          <div
-            key={vehicleType}
-            className={classnames('vehicle-type', {selected: currentSelection === vehicleType})}
-            onClick={() => this.selectVehicle( playerIndex, vehicleType )}
-          >
-            {vehicleType}
-          </div>
-        ))}
-      </div>
-    )
-  }
 
   render() {
-    const { showVehicleSelection } = this.state;
     return (
-      <div className='SplashPage'>
-        {showVehicleSelection ? (
-          <>
-            {this.renderVehicleSelector(1)}
-            {this.renderVehicleSelector(2)}
-            <div
-              className='start-match'
-              onClick={this.startMatch}
-            >
-                Start
-            </div>
-          </>
-        ) : (
-          <>
-            <div className='title'>
-              <div className='title-row'>DUMB</div>
-              <div className='title-row'>RACER</div>
-            </div>
-            <div
-              className='start-selection'
-              onClick={this.startSelection}
-            >
-              • press start •
-            </div>
-        </>
-      )}
+      <div className='SplashPage' onClick={this.start}>
+        <div className='title'>
+          <div className='title-row'>American</div>
+          <div className='title-row'>Demolition</div>
+        </div>
+        <div className='start'>
+          • press start •
+        </div>
       </div>
     );
   }
