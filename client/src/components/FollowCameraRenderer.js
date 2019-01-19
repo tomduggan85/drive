88ccd78851054f3d4013471e705fed9ae956491a
Tom Duggan <tduggan@squarespace.com>
2019-01-19 14:40:56 -0500

    simpler follow cam

--- client/src/components/FollowCameraRenderer.js
+++ client/src/components/FollowCameraRenderer.js
@@ -1,9 +1,10 @@
-/* global THREE Physijs */
+/* global THREE */
 
 import CameraRenderer from './CameraRenderer'
 
 const DEFAULT_FOLLOW_DIST = 45;
 const FOLLOW_HEIGHT = 15;
+const LOOK_HEIGHT = 8;
 
 class FollowCameraRenderer extends CameraRenderer {
 
@@ -16,43 +17,17 @@ class FollowCameraRenderer extends CameraRenderer {
     this.vehicle = this.props.scene.vehicles[this.props.vehicleIndex];
     this.$followObject = this.vehicle.$chassis;
     this.followDistance = this.vehicle.followDistance || DEFAULT_FOLLOW_DIST
+
   }
 
   stepFollow() {
-
-    /*
-      TODO #1: Rewrite this to use a forward vector from $followObject's world transform, flatten that (zero the Y-component),
-      and use that for targetAngle, as $followObject.rotation.y has odd range problems (gimbal lock?).
-
-      TODO #2: Incorporate angular velocity into target rotation, so the camera yaw "lags" a little bit as the vehicle swivels
-
-      TODO #3: Fix the bug where the camera flips to face the front of the vehicle when the vehicle is upside down (relates to #1).
-    */
+    this.$followObject.updateMatrixWorld()
     const { position } = this.$followObject;
-    const rotation = this.$followObject.rotation.y;
-    let targetRotation = -rotation// - 0.4;
 
-    if ( Math.abs( this.$followObject.rotation.x)  > Math.PI/2 ) {
-      //Above-mentioned range issues
-      targetRotation = Math.PI + rotation;
-    }
-
-    /*const angularVelocity = this.$followObject.getAngularVelocity().y;
-    const threshold = 0.5;
-    if (angularVelocity > threshold) {
-      targetRotation += (angularVelocity-threshold) * 0.25;
-    }
-    else if (angularVelocity < -threshold) {
-      targetRotation += (angularVelocity+threshold) * 0.25; 
-    }*/
+    const targetPosition = this.$followObject.localToWorld(new THREE.Vector3(-this.followDistance, FOLLOW_HEIGHT, 0))
+    this.$camera.position.copy(targetPosition)
     
-    this.$camera.position.set(
-      position.x + Math.cos(targetRotation) * -this.followDistance,
-      position.y + FOLLOW_HEIGHT, 
-      position.z + Math.sin(targetRotation) * -this.followDistance
-    );
-
-    const lookPosition = new THREE.Vector3( position.x, position.y + 8, position.z )
+    const lookPosition = new THREE.Vector3( position.x, position.y + LOOK_HEIGHT, position.z )
     this.$camera.lookAt( lookPosition )
   }
 
@@ -60,6 +35,7 @@ class FollowCameraRenderer extends CameraRenderer {
     if ( !this.state.firstPersonMode ) {
       this.stepFollow();
     }
+
     super.step();
   }
 
