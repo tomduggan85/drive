6982d0871aedef449a6af2f7e53bd86d0912811a
Tom Duggan <tduggan@squarespace.com>
2019-01-31 20:55:03 -0500

    more deform WIP

--- client/src/shared/MeshDeformer.js
+++ client/src/shared/MeshDeformer.js
@@ -4,6 +4,24 @@ const UPWARD_DEFORM_BIAS = 16 //bias deformation direction slightly upwards
 const MAX_AFFECTED_DISTANCE_FROM_CONTACT_POINT = 7 * 7 //Vertices greater than this distance from a contact point are unaffected.  Stored squared as an optimization
 const MAX_DEFORMATION = 2.3 //Max distance a vertex moves in one deform call
 const DEFORMATION_STRENGTH = 0.04 //Scalar strength applied to deformVector
+const NOISE = 0.1 //Each vertex calculation is scaled by a random amount, +- noise value
+
+
+const applyNoise = vector => {
+  vector.x *= (1 - NOISE) + 2*Math.random()*NOISE
+  vector.y *= (1 - NOISE) + 2*Math.random()*NOISE
+  vector.z *= (1 - NOISE) + 2*Math.random()*NOISE
+}
+
+const calculateVertexDeformation = ( deformVector, distanceToContactPoint ) => {
+  const distanceFalloff = 1 - 0.5 * ( distanceToContactPoint / MAX_AFFECTED_DISTANCE_FROM_CONTACT_POINT ) //as distance goes from 0 to max, falloff drops from 1 to 0.5
+
+  return new THREE.Vector3()
+    .copy( deformVector ) //Start with deformVector
+    .multiplyScalar(-DEFORMATION_STRENGTH) //Negate the direction and scale based on strength
+    .clampLength(0, MAX_DEFORMATION) //Clamp to max deformation
+    .multiplyScalar(distanceFalloff) //After clamp, scale based on distance from contact point
+}
 
 export const deform = ( $rootObject, contactPoints, deformVector ) => {
 
@@ -28,16 +46,11 @@ export const deform = ( $rootObject, contactPoints, deformVector ) => {
         const closestDistance = Math.min( ...contactPoints.map(cp => vertexWorldPos.distanceToSquared( cp )))
         
         if ( closestDistance <= MAX_AFFECTED_DISTANCE_FROM_CONTACT_POINT ) {
-          const distanceFalloff = 1 - 0.5 * ( closestDistance / MAX_AFFECTED_DISTANCE_FROM_CONTACT_POINT ) //as distance goes from 0 to max, falloff drops from 1 to 0.5
-
-          const vertexDeflection = new THREE.Vector3()
-            .copy( deformVector ) //Start with deformVector
-            .multiplyScalar(-DEFORMATION_STRENGTH) //Negate the direction and scale based on strength
-            .clampLength(0, MAX_DEFORMATION) //Clamp to max deformation
-            .multiplyScalar(distanceFalloff) //After clamp, scale based on distance from contact point
+          const vertexDeformation = calculateVertexDeformation( deformVector, closestDistance)
+          applyNoise( vertexDeformation )
 
           //Apply deformation to vertex, transform back to local space, save changes back to BufferGeometry
-          vertexWorldPos.add(vertexDeflection)
+          vertexWorldPos.add(vertexDeformation)
           const vertexLocalPos = object.worldToLocal(vertexWorldPos)
           geometry.attributes.position.setXYZ(i, vertexLocalPos.x, vertexLocalPos.y, vertexLocalPos.z)
         }
